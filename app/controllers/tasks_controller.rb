1fb15e2fd81df4782020d70406b1359fcf423a85
Michael McKinstry <michael.mckinstry97@gmail.com>
2022-05-24 18:15:28 +1000

    finished tasks site

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,40 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @new_task = Task.new
+  end
+
+  def create
+    @create_task = Task.new(task_params)
+    @create_task.save
+    redirect_to task_path(@create_task)
+  end
+
   def show
     @task = Task.find(params[:id])
+  end
+
+  def edit
+    @edit_task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def delete
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to task_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_24_020613) do
+ActiveRecord::Schema.define(version: 2022_05_24_054615) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
